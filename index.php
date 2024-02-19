e8e8bbfe5e11c6c9cd01d1ab6d24809076f2e51b
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-02-19 23:57:05 +0100

    Baseline HTML code

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>My PHP Website</title>
+</head>
+
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">Becki's Playground</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6">
+      <h2 class="text-2xl font-semibold mb-4">My Website</h2>
+      <p>A place for me to play around with PHP </p>
+    </div>
+  </div>
+</body>
+
+</html>
