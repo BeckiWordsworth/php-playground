cdada3cddbee19ddcc499c16614d7d8ff75d33c3
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-02-20 00:48:51 +0100

    Adding variables

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+// variables
+$title = "My PHP Website";
+$subTitle = "Becki's Playground";
+$heading = "My Website";
+$body = "<p>A place for me to play around with PHP </p>";
+
+$firstName = "Becca";
+$lastName = "Wordsworth";
+
+$fullName = $firstName . ' ' . $lastName;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,19 +18,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>My PHP Website</title>
+  <title>
+    <?=
+      $title;
+    ?>
+  </title>
 </head>
 
 <body class="bg-gray-100">
   <header class="bg-blue-500 text-white p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">Becki's Playground</h1>
+      <h1 class="text-3xl font-semibold">
+        <?= $subTitle ?>
+      </h1>
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
-      <h2 class="text-2xl font-semibold mb-4">My Website</h2>
-      <p>A place for me to play around with PHP </p>
+      <h2 class="text-2xl font-semibold mb-4">
+        <!-- Output -->
+        <?=
+          $heading;
+        ?>
+      </h2>
+      <?=
+        "Hello, my name is $fullName";
+      ?>
     </div>
   </div>
 </body>
